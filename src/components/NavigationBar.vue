2f2dfe9db5a8e9ca844793e62cca3859ea7a5de0
Jian Lik Ng <jng78@gatech.edu>
2023-11-19 03:54:56 +0000

    Creating About Me page

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,25 +2,27 @@
   <div class="flex">
     <ul class="flex flex-grow bg-gradient-to-r from-violet-800 to-indigo-300">
       <li class="flex li-link">
-        <a href="#" class="a-link flex flex-grow items-center justify-center">Home</a>
+        <router-link href="#" class="a-link flex flex-grow items-center justify-center">Home</router-link>
       </li>
       <li class="flex li-link">
-        <a href="#" class="a-link flex flex-grow items-center justify-center">About</a>
+        <router-link href="#" class="a-link flex flex-grow items-center justify-center">About</router-link>
       </li>
       <li class="flex li-link">
-        <a href="#" class="a-link flex flex-grow items-center justify-center">Projects</a>
+        <router-link href="#" class="a-link flex flex-grow items-center justify-center">Projects</router-link>
       </li>
       <li class="flex li-link">
         <a href="#" class="a-link flex flex-grow items-center justify-center">GitHub</a>
       </li>
     </ul>
-    <ul class="grid grid-cols-[50px_230px] w-250px bg-gradient-to-r from-slate-800 to-slate-900 font-bold text-white">
-      <img src="../../src/assets/jl_logo.png" width="50" height="50" class="item1 col-span-1">
-      <div class="grid grid-rows-2 ml-3 mr-3">
-        <strong>Jian Lik Ng</strong>
-        <span>Software Systems Engineer</span>
-      </div>
-    </ul>
+    <div class="w-250px bg-gradient-to-r from-slate-800 to-slate-900">
+      <ul class="grid grid-cols-[75px_230px] font-bold text-white ml-3">
+        <img src="../../src/assets/jl_logo.png" width="50" height="50" class="item1 col-span-1">
+        <div class="grid grid-rows-2 mr-3">
+          <strong>Jian Lik Ng</strong>
+          <span>Software Systems Engineer</span>
+        </div>
+      </ul>
+    </div>
   </div>
 </template>
 
