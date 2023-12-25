e4b7f7601dc6c67a734ab8ce82caa88027d76175
Jian Lik Ng <jng78@gatech.edu>
2023-12-25 03:54:40 +0000

    Can transition using gsap... can we just use plan animation to slide a component?

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -9,11 +9,12 @@
       <button class="button" @click="slide_left">Back</button>
     </div>
     <div class="flex flex-grow overflow-x-hidden w-3/5 border-2 border-pink-400">
-      <Transition 
+      <Transition mode = "out-in"
         v-on:before-enter="beforeEnter"
         v-on:enter="enter"
         v-on:leave="leave">
         <Slide class="" ref="slide1" v-if="slideIndex==1"></Slide>
+        <Slide class="" ref="slide2" v-else-if="slideIndex==2"></Slide>
       </Transition>
     </div>
   </div>
@@ -43,35 +44,25 @@
       beforeEnter(el) {
         console.log("before enter!")
         gsap.set(el, {
-          scaleX: 0.8,
-          scaleY: 1.2
+          x:-700,
+          opacity: 0
         })
         el.style.opacity = 0
       },
       enter(el, done) {
         console.log("enter!")
         gsap.to(el, {
-          duration: 1,
-          scaleX: 1.5,
-          scaleY: 0.7,
+          duration: 0.5,
+          x: 0,
           opacity: 1,
-          x: 150,
-          ease: 'elastic.inOut(2.5, 1)',
           onComplete: done
         })
         el.style.opacity = 1
       },
       leave(el, done) {
         gsap.to(el, {
-          duration: 0.7,
-          scaleX: 1,
-          scaleY: 1,
-          x: 300,
-          ease: 'elastic.inOut(2.5, 1)'
-        })
-        gsap.to(el, {
-          duration: 0.2,
-          delay: 0.5,
+          duration: 0.5,
+          x: 700,
           opacity: 0,
           onComplete: done
         })
