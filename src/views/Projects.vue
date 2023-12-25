4919b38f596383e732c579d33b37f7b0087d1171
Jian Lik Ng <jng78@gatech.edu>
2023-12-25 04:44:11 +0000

    Scroll workedgit add srcgit add src

--- src/views/Projects.vue
+++ src/views/Projects.vue
@@ -8,32 +8,39 @@
       <button type="button" @click="slide_right" >Forward</button>
       <button class="button" @click="slide_left">Back</button>
     </div>
-    <div class="flex flex-grow overflow-x-hidden w-3/5 border-2 border-pink-400">
-      <Transition mode = "out-in"
-        v-on:before-enter="beforeEnter"
-        v-on:enter="enter"
-        v-on:leave="leave">
-        <Slide class="" ref="slide1" v-if="slideIndex==1"></Slide>
-        <Slide class="" ref="slide2" v-else-if="slideIndex==2"></Slide>
-      </Transition>
+    <div class="overflow-auto flex flex-grow w-3/5 border-2 border-pink-400" ref="horizontal">
+      <Slide class="flex-shrink-0" ref="slide1"></Slide>
+      <Slide class="flex-shrink-0" ref="slide2"></Slide>
     </div>
   </div>
 </template>
 
 <script>
   import gsap from 'gsap';
+  import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+  gsap.registerPlugin(ScrollToPlugin);
 
   export default {
     data() {
       return {
-        slideIndex: 0
+        slideIndex: 0,
+        scrollPos: 0,
+        horizontalContainer: Object
       }
     },
     mounted() {
       this.slideIndex = 1;
+      this.horizontalContainer = this.$refs.horizontal;
     },
     methods: {
       slide_right() {
+        // this.$nextTick(() => {
+        //   this.$refs.horizontal.animate({scrollLeft: this.scrollPos + 100});
+        //   this.scrollPos = this.scrollPos + 100;
+        // })
+        console.log(this.horizontalContainer);
+        gsap.to(this.horizontalContainer, { scrollTo: {x: 250}, duration: 2, ease: 'power2.inOut' });
+
         this.slideIndex = this.slideIndex + 1;
         console.log(this.slideIndex);
       },
